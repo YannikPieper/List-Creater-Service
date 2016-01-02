87b7d2aa0c27a33c488bc6a473149c508d93260c
Jonathan Burggraf <jonathan@Jonathans-Computer.fritz.box>
2016-01-02 18:15:32 +0100

    Add finishing touches

--- index.js
+++ index.js
@@ -9,26 +9,38 @@ var conString = 'postgres://localhost:5432/jonathan';
 
 app.use(bodyParser.json());
 
-app.post('/api/v1/search',echo,db_search,end);
+app.post('/api/v1/search',query_creater,db_search,end);
 
-function echo(req,res,next){
+function query_creater(req,res,next){
   console.dir(req.body);
-  //var data = req.body;
-  next();
+  var query = 'SELECT id, name, size FROM list_creater_service WHERE';
+  if(req.body.max_size){
+    query = query + ' size <=' + req.body.max_size + ' AND';
+  }
+  if(req.body.min_size){
+    query = query + ' size >=' + req.body.min_size + ' AND';
+  }
+  if(req.body.gamemode){
+    query = query + ' ' + req.body.gamemode + ' = ANY(gamemode)' + ' AND';
+  }
+  if(req.body.country){
+    query = query + ' country = '  + req.body.country + ' AND';
+  }
+  next(query.slice(0, -4));
 }
 
 function end(req,res){
   res.end;
 }
 
-function db_search(req,res,next){
-  var a = 'SELECT id, name, size FROM list_creater_service WHERE size <=' + req.body.size.max + 'AND size >= ' + req.body.size.min + 'AND ' + req.body.gamemode + ' = ANY(gamemode) AND country = '  + req.body.country;
-  console.log(a);
+function db_search(qry,req,res,next){
+  //var a = 'SELECT id, name, size FROM list_creater_service WHERE size <=' + req.body.size.max + 'AND size >= ' + req.body.size.min + 'AND ' + req.body.gamemode + ' = ANY(gamemode) AND country = '  + req.body.country;
+  console.log(qry);
   pg.connect(conString, function(err, client, done) {
     if(err) {
       return console.error('error fetching client from pool', err);
     }
-    client.query(a,function(err, result) {
+    client.query(qry,function(err, result) {
       done();
       if(err) {
         return console.error('error running query', err);
